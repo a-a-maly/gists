afce24861f8443a88a14533b75079e6e884e719f
Alexander A. Maly <a.a.maly@gmail.com>
2017-02-16 22:20:59 +0300

    Create read_uint.cpp
    For reading and parsing numbers.

---
+++ io/read_uint.cpp
@@ -0,0 +1,19 @@
+typedef unsigned int uint_t;
+
+uint_t read_uint()
+{
+	const uint_t D = 10;
+	uint_t u = 0;
+	do {
+		c = getchar_unlocked();
+		assert (0 <= c);
+		u = c - '0';
+	} while (D <= u);
+	for (;;) {
+		uint_t v = getchar_unlocked() - '0';
+		if (D <= v)
+			break;
+		u = u * D + v;
+	}
+	return u;
+}
