79fb97ca266474940ab987e1d760acdb5dc757c3
Alexander A. Maly <a.a.maly@gmail.com>
2019-02-12 21:01:08 +0300

    added signed and 64-bit unsigned parsing

--- io/read_uint.cpp
+++ io/read_uint.cpp
@@ -1,3 +1,4 @@
+#include <inttypes.h>
 typedef unsigned int uint_t;
 
 static uint_t read_uint()
@@ -16,4 +17,44 @@ static uint_t read_uint()
 		u = u * D + v;
 	}
 	return u;
+}
+
+static uint64_t read_uint64()
+{
+	const uint_t D = 10;
+	uint64_t u = 0;
+	do {
+		int c = getchar_unlocked();
+		assert (0 <= c);
+		u = c - '0';
+	} while (D <= u);
+	for (;;) {
+		uint_t v = getchar_unlocked() - '0';
+		if (D <= v)
+			break;
+		u = u * D + v;
+	}
+	return u;
+}
+
+static int read_int()
+{
+	const uint_t D = 10;
+	uint_t u = 0, s = 0;
+	do {
+		int c = getchar_unlocked();
+		assert (0 <= c);
+		u = c - '0';
+		if (c == '-') {
+			s = 1;
+			u = 0;
+		}
+	} while (D <= u);
+	for (;;) {
+		uint_t v = getchar_unlocked() - '0';
+		if (D <= v)
+			break;
+		u = u * D + v;
+	}
+	return s ? -u : u;
 }
